a3d2a539e8a08283abdb4d54840ce80269aa3c95
Karmele <ktelletxea.kt@gmail.com>
2024-06-07 10:49:47 +0200

    popwrap funciona

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const condiciones  = document.getElementById("condiciones");
 const enviar = document.getElementById("enviar");
-
 const cambiarCondiciones = () => condiciones.checked ? enviar.disabled = false : enviar.disabled = true
+
+    function pop(title, text) {
+        document.getElementById("poptitle").innerHTML = title;
+        document.getElementById("poptext").innerHTML = text;
+        document.getElementById("popwrap").classList.add('show');
+    }
+    
+    function hidePop() {
+        document.getElementById("popwrap").classList.remove('show');
+    }
