f3b03a370eea4451e2da2585dd9290f882ca2028
Karmele <ktelletxea.kt@gmail.com>
2024-06-07 12:36:16 +0200

    meter imagenes de prueba, empezar con estilos de fotos y index fotos.

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
 //funcion boton 1- disabled y cheked y location nuevo
 const condiciones  = document.getElementById("condiciones");
 const enviar = document.getElementById("enviar");
-
+//condiciones ?
 const cambiarCondiciones = () => { enviar.disabled = !condiciones.checked;};
-
+//mandame a otra pagina
 condiciones.addEventListener('change', cambiarCondiciones);
 
 enviar.addEventListener('click', function(event) {
@@ -13,12 +12,13 @@ enviar.addEventListener('click', function(event) {
 });
 
 // Funcion POPUP
+    //funcion aparecer 
     function pop(title, text) {
         document.getElementById("poptitle").innerHTML = title;
         document.getElementById("poptext").innerHTML = text;
         document.getElementById("popwrap").classList.add('show');
     }
-    
+     //funcion desaparecer 
     function hidePop() {
         document.getElementById("popwrap").classList.remove('show');
     }
