1b64dd9e386573637156e7f1f03e8249ac543ebb
Karmele <ktelletxea.kt@gmail.com>
2024-06-07 11:47:29 +0200

    boton funcional

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+
+//funcion boton 1- disabled y cheked y location nuevo
 const condiciones  = document.getElementById("condiciones");
 const enviar = document.getElementById("enviar");
-const cambiarCondiciones = () => condiciones.checked ? enviar.disabled = false : enviar.disabled = true
 
+const cambiarCondiciones = () => { enviar.disabled = !condiciones.checked;};
+
+condiciones.addEventListener('change', cambiarCondiciones);
+
+enviar.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'fotos.html';
+});
+
+// Funcion POPUP
     function pop(title, text) {
         document.getElementById("poptitle").innerHTML = title;
         document.getElementById("poptext").innerHTML = text;
@@ -11,3 +22,5 @@ const cambiarCondiciones = () => condiciones.checked ? enviar.disabled = false :
     function hidePop() {
         document.getElementById("popwrap").classList.remove('show');
     }
+
+        
